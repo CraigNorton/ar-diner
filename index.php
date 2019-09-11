365ab70ab3b8d13970bd94733ba0fd5a96e5d4d1
CraigNorton <craigbnorton@gmail.com>
2019-09-11 00:38:02 -0400

    adding the hotdog

--- index.php
+++ index.php
@@ -4,14 +4,21 @@
 <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.7.2/aframe/build/aframe-ar.js"></script>
   <body style='margin : 0px; overflow: hidden;'>
     <a-scene embedded arjs>
+	  <a-assets>
+		<a-asset-item id="crate-obj" src="objHotdog.obj"></a-asset-item>
+		<a-asset-item id="crate-mtl" src="objHotdog.mtl"></a-asset-item>
+	  </a-assets>
+	  
       <a-marker preset="hiro">
           <a-box position='0 0.5 0' material='color: yellow;'></a-box>
       </a-marker>
 	
 	<a-marker preset='kanji'>
-		<a-box position='0 0.5 0' material='color: red;'></a-box>
+		<a-obj-model src="#crate-obj" mtl="#crate-mtl"></a-obj-model>
 	</a-marker>
-      <a-entity camera></a-entity>
+    <a-entity camera></a-entity>
     </a-scene>
+	<button style="position:absolute; left:500px; top:500px;" onclick="alert('hello');">Left</button>
+	<button>Right</button>	
   </body>
 </html>
