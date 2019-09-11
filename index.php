8fe80b8674ec5058dcfe7fcea666e3d22e6ed1ac
CraigNorton <craigbnorton@gmail.com>
2019-09-11 01:01:51 -0400

    adding new logarithmicDepthBuffer

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!doctype HTML>
 <html>
+<!--
+<a-scene embedded arjs>
+
+<a-entity camera></a-entity>
+-->
 <script src="https://aframe.io/releases/0.9.1/aframe.min.js"></script>
 <script src="https://cdn.rawgit.com/jeromeetienne/AR.js/1.7.2/aframe/build/aframe-ar.js"></script>
   <body style='margin : 0px; overflow: hidden;'>
     <a-scene embedded arjs>
+	<a-scene embedded artoolkit='sourceType: webcam;' renderer='logarithmicDepthBuffer: true;'>
 	  <a-assets>
 		<a-asset-item id="crate-obj" src="objHotdog.obj"></a-asset-item>
 		<a-asset-item id="crate-mtl" src="objHotdog.mtl"></a-asset-item>
@@ -16,7 +22,7 @@
 	<a-marker preset='kanji'>
 		<a-obj-model src="#crate-obj" mtl="#crate-mtl" scale=".1 .1 .1"></a-obj-model>
 	</a-marker>
-    <a-entity camera></a-entity>
+    <a-marker-camera camera="far:100000; near:0.01" >
     </a-scene>
 	<button style="position:absolute; left:500px; top:500px;" onclick="alert('hello');">Left</button>
 	<button>Right</button>	
